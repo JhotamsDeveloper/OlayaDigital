df521f906f4bc37f1ddc5f5e6263ab1b36c3e400
JhotaMSdeveloper <jhotams.developer@gmail.com>
2021-02-07 00:43:21 -0500

    Probando repositorio Post

---
+++ src/OlayaDigital.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,26 @@
+﻿using OlayaDigital.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OlayaDigital.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var _post = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                Id = x,
+                Tittle = $"Titulo {x}",
+                Url = $"Url {x}",
+                Description = $"Descripcion {x}",
+                IdCategoria =  x * 3,
+                IdUsuario = x * 5
+            });
+
+            return _post;
+        }
+    }
+}
---
+++ src/OlayaDigitalAPI/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlayaDigital.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OlayaDigitalAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            //Forma de traer una clase sin hacer una inyección de dependencia
+            var _posts = new PostRepository().GetPosts();
+
+            return Ok(_posts);
+        }
+    }
+}
