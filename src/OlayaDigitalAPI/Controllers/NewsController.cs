6876b023fdb363b23c2e5bc3d1ac81abc12a4215
JhotaMSdeveloper <jhotams.developer@gmail.com>
2021-04-10 18:29:03 -0500

    Implementacion de noticias en la API

---
+++ src/OlayaDigitalAPI/Controllers/NewsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+
+namespace OlayaDigitalAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NewsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public NewsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Article>> National()
+        {
+            var apiKey = _configuration.GetValue<string>("NewsAPI:apiKey");
+
+            var newsApiClient = new NewsApiClient(apiKey);
+
+            var articlesResponseAsync = newsApiClient.GetTopHeadlinesAsync(new TopHeadlinesRequest
+            {
+                Country = Countries.CO,
+                Language = Languages.ES,
+                Category = Categories.Science,
+                PageSize = 5,
+            });
+
+            var articlesResult = await articlesResponseAsync;
+
+            List<Article> listNewsNational = new List<Article>();
+
+            foreach (var item in articlesResult.Articles)
+            {
+                Article article = new Article();
+                //article.Source = item.Source;
+                article.Author = item.Author;
+                article.Title = item.Title;
+                article.Description = item.Description;
+                article.Url = item.Url;
+                article.UrlToImage = item.UrlToImage;
+                article.PublishedAt = item.PublishedAt;
+                article.Content = item.Content;
+
+                listNewsNational.Add(article);
+            }
+            return listNewsNational;
+        }
+    }
+}
