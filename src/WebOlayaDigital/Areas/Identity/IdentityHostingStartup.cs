bc7dbc104634ed88d5f209c7ef91fd690b96dd8d
JhotaMSdeveloper <jhotams.developer@gmail.com>
2021-04-10 21:56:23 -0500

    Implementación de la pagina web

---
+++ src/WebOlayaDigital/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebOlayaDigital.Data;
+
+[assembly: HostingStartup(typeof(WebOlayaDigital.Areas.Identity.IdentityHostingStartup))]
+namespace WebOlayaDigital.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
